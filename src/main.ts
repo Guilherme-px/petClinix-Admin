3d64bb4be8053cdbe4a8d68589fc0b63767f125b
Guilherme-px <guilherme.s.goncalves@outlook.com>
2026-08-25 18:38:57 -0300

    chore: update App.vue and main.ts with Quasar and login page setup

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,20 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import { Quasar } from "quasar";
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
+import router from "./router";
 
-import { Quasar } from 'quasar'
 import "@quasar/extras/material-icons/material-icons.css";
-import 'quasar/src/css/index.sass'
+import "quasar/src/css/index.sass";
+import "@/assets/css/main.css";
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(createPinia())
-app.use(router)
+app.use(createPinia());
+app.use(router);
 app.use(Quasar, {
     plugins: {},
 });
 
-app.mount('#app')
+app.mount("#app");
