2e1a87081ac596a01641f308ce0ce4c3770c461f
Guilherme-px <guilherme.s.goncalves@outlook.com>
2026-08-25 09:35:54 -0300

    chore: integrate Quasar UI framework with Vite

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+    <h1>You did it!</h1>
+    <p>
+        Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+        documentation
+    </p>
+
+    <div style="padding: 20px">
+        <q-btn color="primary" icon="check" label="Quasar está funcionando!" />
+    </div>
 </template>
 
 <style scoped></style>
