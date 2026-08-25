9225b1371d5172beb94a412d1c56b4a99be7b8b1
Guilherme-px <guilherme.s.goncalves@outlook.com>
2026-08-25 18:27:41 -0300

    chore: setup Quasar UI, brand colors and CSS variables

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 import { fileURLToPath, URL } from "node:url";
-
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 import vueDevTools from "vite-plugin-vue-devtools";
 import { quasar, transformAssetUrls } from "@quasar/vite-plugin";
 
-// https://vite.dev/config/
 export default defineConfig({
     plugins: [
         vue({
@@ -14,7 +12,9 @@ export default defineConfig({
         }),
         vueJsx(),
         vueDevTools(),
-        quasar(),
+        quasar({
+            sassVariables: "@/assets/css/quasar.variables.sass",
+        }),
     ],
     resolve: {
         alias: {
