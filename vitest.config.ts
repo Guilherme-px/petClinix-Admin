c8ca2275495304bfc8b41fc3eed7d87e338fb284
Guilherme-px <guilherme.s.goncalves@outlook.com>
2026-08-25 20:11:36 -0300

    chore: add testing dependencies and setup vitest for quasar

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,9 +6,11 @@ export default mergeConfig(
   viteConfig,
   defineConfig({
     test: {
-      environment: 'jsdom',
+      environment: 'happy-dom', 
       exclude: [...configDefaults.exclude, 'e2e/**'],
       root: fileURLToPath(new URL('./', import.meta.url)),
+      globals: true,
+      setupFiles: ['src/test/setup.ts']
     },
   }),
 )
