b7056313bb7822f2c87cd2d4f89877fe326c7f3a
Guilherme-px <guilherme.s.goncalves@outlook.com>
2026-09-16 08:14:28 -0300

    feat: add mask composable

---
+++ src/composables/useMasks.ts
@@ -0,0 +1,13 @@
+export const useMasks = () => {
+    const phoneMask = "(##) #####-####";
+    const cpfMask = "###.###.###-##";
+    const cnpjMask = "##.###.###/####-##";
+    const cepMask = "#####-###";
+
+    return {
+        phoneMask,
+        cpfMask,
+        cnpjMask,
+        cepMask,
+    };
+};
