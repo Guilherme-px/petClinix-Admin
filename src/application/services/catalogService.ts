d959988f2c9f1407480afe985c1a6f6792cf6fad
Guilherme-px <guilherme.s.goncalves@outlook.com>
2026-09-22 09:17:54 -0300

    feat: add catalog services

--- src/application/services/catalogService.ts
+++ src/application/services/catalogService.ts
@@ -1,6 +1,6 @@
 import type { IServiceRepository } from "@/domain/repositories/IServiceRepository";
 import type { VeterinaryService } from "@/domain/models/VeterinaryService";
-import type { FetchParams, PaginatedResponse } from "@/domain/models/common";
+import type { FetchParams, PaginatedResponse } from "@/domain/models/pagination";
 
 export const createCatalogService = (serviceRepository: IServiceRepository) => {
     return {
